57d04834bcef5275b9cde74e27330ea5f33893cc
Ryan Wong <ryanjameswong@outlook.com>
2024-08-21 13:58:37 -0400

    add sky background

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Ball from "./components/Ball";
 import Ground from "./components/Ground";
 import Tortoise from "./components/Tortoise";
 import { useRef, useState } from "react";
+import { Sky, Text } from "@react-three/drei";
 import * as THREE from "three";
 
 function App() {
@@ -28,50 +29,62 @@ function App() {
   };
 
   return (
-      <Canvas
-        camera={{
-          fov: 60,
-          position: [1, 3, 8],
-          rotation: [Math.PI / 90, 0, 0], // Rotate camera 45 degrees on x and y axes
-          zoom: 1, // Default zoom
-        }}
-        shadows
-        resize={{ debounce: 0 }}
-      >
-        {/* <color attach="background" args={['#171720']} /> */}
-        <ambientLight intensity={2} />
-        {/* <pointLight decay={2} intensity={1} position={[0, 0, 0]} /> */}
-        <spotLight
-          angle={0.6}
-          castShadow
-          decay={0}
-          intensity={Math.PI}
-          penumbra={1}
-          position={[10, 10, 10]}
-          shadow-mapSize-width={2048}
-          shadow-mapSize-height={2048}
-          shadow-bias={-0.0001}
+    <Canvas
+      camera={{
+        fov: 60,
+        position: [1, 3, 8],
+        rotation: [(2 * Math.PI) / 180, 0, 0],
+        zoom: 1,
+      }}
+      shadows
+      resize={{ debounce: 0 }}
+    >
+      <Sky
+        distance={450000}
+        sunPosition={[100, 10, 100]}
+        inclination={0.49}
+        azimuth={0.25}
+      />
+      <ambientLight intensity={2} />
+      <spotLight
+        angle={0.6}
+        castShadow
+        decay={0}
+        intensity={Math.PI}
+        penumbra={1}
+        position={[10, 10, 10]}
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+        shadow-bias={-0.0001}
+      />
+      <Physics gravity={[0, -30, 0]}>
+        <Ball
+          xyPlane={xyPlane}
+          updateBallThrown={updateBallThrown}
+          ref={ballRef}
         />
-        <Physics
-          gravity={[0, -30, 0]}
-          // Rapier specific configuration if needed
+        <Tortoise
+          ballRef={ballRef}
+          thrown={thrown}
+          caught={caught}
+          handleCaughtBall={handleCaughtBall}
+          handleReturnBall={handleReturnBall}
+          ref={tortoiseRef}
+        />
+        <Ground />
+      </Physics>
+      {!thrown && (
+        <Text
+          color="white"
+          fontSize={0.5}
+          position={[0, 5, -5]}
+          anchorX="center"
+          anchorY="middle"
         >
-          <Ball
-            xyPlane={xyPlane}
-            updateBallThrown={updateBallThrown}
-            ref={ballRef}
-          />
-          <Tortoise
-            ballRef={ballRef}
-            thrown={thrown}
-            caught={caught}
-            handleCaughtBall={handleCaughtBall}
-            handleReturnBall={handleReturnBall}
-            ref={tortoiseRef}
-          />
-          <Ground />
-        </Physics>
-      </Canvas>
+          Drag, flick, and release the ball to throw it
+        </Text>
+      )}
+    </Canvas>
   );
 }
 
